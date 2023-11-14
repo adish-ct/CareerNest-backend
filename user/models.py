7215e94d4a69f618d1a77c3312e63d9b2a4a0b5f
adish-ct <adishathu525@gmail.com>
2023-11-14 17:13:48 +0530

    created employer table

--- user/models.py
+++ user/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+
+class Employer(models.Model):
+    employer = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    employer_type = models.CharField(max_length=255, null=True, blank=True)
+    employer_description = models.TextField(null=True, blank=True)
+    employer_location = models.CharField(max_length=255, null=True, blank=True)
+    profile_image = models.ImageField(null=True, blank=True)
+    banner_image = models.ImageField(null=True, blank=True)
+    rating = models.IntegerField(default=5, blank=True)
+    is_verified = models.BooleanField(default=False)
+    avarage_salary = models.IntegerField(default=500000, blank=True)
+    role = models.CharField(default="Employer")
+    head_quarters = models.CharField(blank=True, null=True)
+    phone = models.CharField(max_length=10, null=True, blank=True)
+    followers = models.IntegerField(default=0, blank=True)
+
+    def __str__(self) -> str:
+        return self.employer.username
+    
+
+
+
+    
+
