30a177754f48570f5c61ac541392ed061870d43f
adish-ct <adishathu525@gmail.com>
2023-11-25 09:10:14 +0530

    serializer changed removed create method

--- applications/jobs/serializers/job_serilizers.py
+++ applications/jobs/serializers/job_serilizers.py
@@ -6,8 +6,4 @@ class JobSerializer(ModelSerializer):
     class Meta:
         model = Jobs
         fields = '__all__'
-    def create(self, validated_data):
-        print("-----------------------")
-        print(validated_data)
-        print("-----------------------")
-        return super().create(validated_data)
+   
