3ee14de63fa509f300a9f26a03c87b8fb1924323
adish-ct <adishathu525@gmail.com>
2023-12-05 20:07:15 +0530

    added custom validation for experience model fields

--- applications/proffessional/serializers/experience_serializer.py
+++ applications/proffessional/serializers/experience_serializer.py
@@ -1,8 +1,19 @@
 from rest_framework.serializers import Serializer, ModelSerializer
 from ..models import Experience
+from django.core.exceptions import ValidationError
+
+
+def CustomExperienceFieldValidator(value):
+    if not value.replace(" ", "").isalpha():
+        raise ValidationError('should only conatins alphabets')
 
 class ExpenceSerializer(ModelSerializer):
     class Meta:
         model = Experience
         fields = '__all__'
+        extra_kwargs = {
+            'job_role': {
+                'validators': [CustomExperienceFieldValidator],
+            },
+        }
     
