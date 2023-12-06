4f688134ee98352a60803dc1423f37932c6d312e
adish-ct <adishathu525@gmail.com>
2023-12-06 10:34:09 +0530

    validate function implemented

--- applications/proffessional/serializers/experience_serializer.py
+++ applications/proffessional/serializers/experience_serializer.py
@@ -1,4 +1,5 @@
 from typing import Any
+from rest_framework import serializers
 from rest_framework.serializers import Serializer, ModelSerializer
 from ..models import Experience
 from django.core.exceptions import ValidationError
@@ -29,4 +30,15 @@ class ExpenceSerializer(ModelSerializer):
             'start_date': {'validators': [CustomDateValidator()],},
             'end_date': {'validators': [CustomDateValidator()],},
         }
+
+        
+    def validate(self, data):
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+
+        if start_date and end_date and start_date > end_date:
+            raise serializers.ValidationError("Start date must be less than end date")
+        return data
+
+
     
