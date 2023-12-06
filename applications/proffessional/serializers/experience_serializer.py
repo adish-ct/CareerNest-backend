3d118bea1ffcdc06367bca822c562f509e66939d
adish-ct <adishathu525@gmail.com>
2023-12-06 10:03:32 +0530

    added custom validators for date field

--- applications/proffessional/serializers/experience_serializer.py
+++ applications/proffessional/serializers/experience_serializer.py
@@ -1,19 +1,32 @@
+from typing import Any
 from rest_framework.serializers import Serializer, ModelSerializer
 from ..models import Experience
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
-def CustomExperienceFieldValidator(value):
-    if not value.replace(" ", "").isalpha():
-        raise ValidationError('should only conatins alphabets')
+class CustomFieldValidator:
+    def __call__(self, value):
+        if not value.replace(" ", "").isalpha():
+            raise ValidationError("Should contain alphabets only")
+
+
+class CustomDateValidator:
+    
+    def __call__(self, date ):
+        if date and date > timezone.now().date():
+            raise ValidationError("Invalid date input")
+    
 
 class ExpenceSerializer(ModelSerializer):
     class Meta:
         model = Experience
         fields = '__all__'
         extra_kwargs = {
-            'job_role': {
-                'validators': [CustomExperienceFieldValidator],
-            },
+            'job_role': {'validators': [CustomFieldValidator()],},
+            'organization': {'validators': [CustomFieldValidator()],},
+            'location': {'validators': [CustomFieldValidator()],},
+            'start_date': {'validators': [CustomDateValidator()],},
+            'end_date': {'validators': [CustomDateValidator()],},
         }
     
