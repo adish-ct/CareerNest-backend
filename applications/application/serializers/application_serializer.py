0b24d18299e2535939fc0d64ec47344df11a1170
adish-ct <adishathu525@gmail.com>
2023-12-17 19:01:02 +0530

    create new serializer for employer application view

--- applications/application/serializers/application_serializer.py
+++ applications/application/serializers/application_serializer.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+from applications.accounts.models import User
+from applications.jobs.models import Jobs
 from applications.accounts.serializers.user_serializer import UserSerializer
+from applications.jobs.serializers.job_serilizers import JobSerializer
 from ..models import Application
 
+
 class ApplicationSerializer(ModelSerializer):
-    user = UserSerializer()
     class Meta:
         model = Application
         fields = ['id', 'user', 'job', 'status', 'is_pending', 'is_accept', 'is_reject']
-        
+
+
+class EmployerApplicationSerializer(ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Application
+        fields = ['id', 'user', 'job', 'status', 'is_pending', 'is_accept', 'is_reject']
+        extra_kwargs = {
+            'user': {'read_only': True}
+        }
